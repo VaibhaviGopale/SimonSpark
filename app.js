e27cad7bdc81957cbc4cfc0d6eb35e02db3c907c
Vaibhavi Gopale <vaibhavi01gopale>
2026-01-16 17:16:15 +0530

    Added sound, restart button, high score and mobile support

--- app.js
+++ app.js
@@ -7,76 +7,104 @@ let started = false;
 let level = 0;
 
 let h2 = document.querySelector("h2");
+let highScoreEl = document.querySelector("#highScore");
+let restartBtn = document.querySelector("#restart");
 
-document.addEventListener("keypress", function() {
-    if (started == false) {
-        console.log("game is started");
-        started = true;
+let highScore = localStorage.getItem("highScore") || 0;
+highScoreEl.innerText = `High Score: ${highScore}`;
 
-        levelUp();
-    }
+const sounds = {
+  red: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
+  green: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
+  yellow: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
+  purple: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"),
+  wrong: new Audio("https://www.soundjay.com/buttons/sounds/button-10.mp3")
+};
+
+function startGame() {
+  if (!started) {
+    started = true;
+    levelUp();
+  }
+}
+
+document.addEventListener("keypress", startGame);
+document.addEventListener("touchstart", startGame);
+
+restartBtn.addEventListener("click", () => {
+  reset();
+  h2.innerText = "Press any key or tap to start";
 });
 
 function gameFlash(btn) {
-    btn.classList.add("gameflash");
-    setTimeout(function() {
-        btn.classList.remove("gameflash");
-    }, 250);
+  btn.classList.add("gameflash");
+  sounds[btn.id].play();
+  setTimeout(() => btn.classList.remove("gameflash"), 250);
 }
 
 function userFlash(btn) {
-    btn.classList.add("userflash");
-    setTimeout(function() {
-        btn.classList.remove("userflash");
-    }, 250);
+  btn.classList.add("userflash");
+  sounds[btn.id].play();
+  setTimeout(() => btn.classList.remove("userflash"), 250);
 }
 
 function levelUp() {
-    userSeq = [];
-    level++;
-    h2.innerText = `level ${level}`;
-    
-    let randIdx = Math.floor(Math.random() * 3);
-    let randColor = btns[randIdx];
-    let randBtn = document.querySelector(`.${randColor}`);
-    gameSeq.push(randColor);
-    console.log(gameSeq);
-    gameFlash(randBtn);
+  userSeq = [];
+  level++;
+  h2.innerText = `Level ${level}`;
+
+  let randIdx = Math.floor(Math.random() * 4);
+  let randColor = btns[randIdx];
+  let randBtn = document.querySelector(`#${randColor}`);
+
+  gameSeq.push(randColor);
+  gameFlash(randBtn);
 }
 
 function checkAns(idx) {
-    if (userSeq[idx] === gameSeq[idx]) {
-      if (userSeq.length == gameSeq.length) {
-        setTimeout(levelUp, 1000);
-      }
-    } else {
-        h2.innerHTML = `Game Over! Your score was <b>${level}</b> <br> Press any key to start.`;
-        documen.querySelector("body").style.backgroundColor = "red";
-        setTimeout(function () {
-           documen.querySelector("body").style.backgroundColor = "white"; 
-        })
-        reset();
+  if (userSeq[idx] === gameSeq[idx]) {
+    if (userSeq.length == gameSeq.length) {
+      setTimeout(levelUp, 1000);
     }
+  } else {
+    sounds.wrong.play();
+
+    if (level > highScore) {
+      highScore = level;
+      localStorage.setItem("highScore", highScore);
+      highScoreEl.innerText = `High Score: ${highScore}`;
+    }
+
+    h2.innerHTML = `Game Over! Your score was <b>${level}</b><br>Press any key or tap to start.`;
+    document.body.style.backgroundColor = "red";
+
+    setTimeout(() => {
+      document.body.style.backgroundColor = "white";
+    }, 300);
+
+    reset();
+  }
 }
 
 function btnPress() {
-    let btn = this;
-    userFlash(btn);
+  let btn = this;
+  userFlash(btn);
 
-    userColor = btn.getAttribute("id");
-    userSeq.push(userColor);
+  let userColor = btn.getAttribute("id");
+  userSeq.push(userColor);
 
-    checkAns(userSeq.length-1);
+  checkAns(userSeq.length - 1);
 }
 
 let allBtns = document.querySelectorAll(".btn");
-for(btn of allBtns) {
-    btn.addEventListener("click", btnPress);
+for (let btn of allBtns) {
+  btn.addEventListener("click", btnPress);
+  btn.addEventListener("touchstart", btnPress);
 }
 
-function reset () {
-    started = false;
-    gameSeq = [];
-    userSeq = [];
-    level = 0;
+function reset() {
+  started = false;
+  gameSeq = [];
+  userSeq = [];
+  level = 0;
 }
